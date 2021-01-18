fc97bcb1f0a9af86e5909cde814c323cf631792d
Mohamed Gamal <gemomy2015@gmail.com>
2021-01-18 14:21:23 +0200

    Update Missing_Numbers.py

--- Hackerrank/Algorithms/Search/Missing_Numbers.py
+++ Hackerrank/Algorithms/Search/Missing_Numbers.py
@@ -6,6 +6,7 @@ import os
 import random
 import re
 import sys
+from collections import Counter
 
 # Complete the missingNumbers function below.
 def missingNumbers(arr, brr):
@@ -16,6 +17,11 @@ def missingNumbers(arr, brr):
     for i in brr:
         list[i] -= 1
     return sorted([item for item in range(len(list)) if list[item] != 0])
+
+# Another solution 
+def missingNumbers(arr, brr):
+    diff = Counter(brr) - Counter(arr)
+    return sorted(diff.keys())
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
